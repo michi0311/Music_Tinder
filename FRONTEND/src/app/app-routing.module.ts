ebe20653b1fec30d855d0d845af5c9b7443bed60
albin <albin.weiss@outlook.com>
2020-01-24 23:20:23 +0100

    INSTALL

--- FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/src/app/app-routing.module.ts
@@ -9,26 +9,26 @@ import {SongListComponent} from './profile/music/song-list/song-list.component';
 import {SongSearchComponent} from './profile/music/song-search/song-search.component';
 import {SongSwipingComponent} from './song-swiping/song-swiping.component';
 import {SettingsComponent} from './profile/settings/settings.component';
-import { PersSettingsComponent } from './profile/settings/pers-settings/pers-settings.component';
+import {PersSettingsComponent} from './profile/settings/pers-settings/pers-settings.component';
 
 
 const routes: Routes = [
   // TODO Albin: Redirecting Default to Login, when not logged in - else Startscreen or Matching Screen
-  { path: '', component: SongSwipingComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  {path: '', component: SongSwipingComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
 
 
   // { path: '', redirectTo: '/song-list', pathMatch: 'full' },
   {path: 'song-list', component: SongListComponent},
   {path: 'song-search', component: SongSearchComponent},
   // TODO Kathi: Add Routes für Swiping and Matching Screen, or Chatting or other
-  { path: 'song-swipe', component: SongSwipingComponent},
-  { path: 'settings', component: SettingsComponent},
+  {path: 'song-swipe', component: SongSwipingComponent},
+  {path: 'settings', component: SettingsComponent},
   {path: 'pers-settings', component: PersSettingsComponent},
 
   // otherwise redirect to home
-  { path: '**', redirectTo: '' },
+  {path: '**', redirectTo: ''},
 ];
 
 @NgModule({
