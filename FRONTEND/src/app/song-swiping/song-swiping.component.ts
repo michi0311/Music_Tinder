23ca276db7363b6f2df1f66548391c57ba147025
Katharina Brugger <kabrugger@outlook.com>
2020-01-25 18:36:13 +0100

    functioning Matching - Song stops when changing routes

--- FRONTEND/src/app/song-swiping/song-swiping.component.ts
+++ FRONTEND/src/app/song-swiping/song-swiping.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {Howl} from 'howler';
 import {SongSwipingService} from "./song-swiping.service";
-import { ToastController } from '@ionic/angular';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-song-swiping',
@@ -9,17 +9,29 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['./song-swiping.component.css']
 })
 
-export class SongSwipingComponent implements OnInit {
+export class SongSwipingComponent implements OnInit, OnDestroy {
   public randomUser;
   public audio;
   public isPlaying = false;
 
-  constructor(private songService: SongSwipingService, public toastCtrl: ToastController) {}
+  constructor(private songService: SongSwipingService, public toastCtrl: ToastController) {
+  }
+
 
   //TODO - Stop song when switching routes!
-  ngOnInit() {
+
+  ngOnInit(): void {
     this.getRandomSong();
   }
+
+  ngOnDestroy() {
+    try {
+      this.audio.stop()
+    } catch (e) {
+
+    }
+  }
+
 
 //get random user with his/her titlesong and play it in the browser
   getRandomSong() {
@@ -33,7 +45,8 @@ export class SongSwipingComponent implements OnInit {
           document.getElementById("divText").innerHTML = this.randomUser.user.name + "<br>" + this.randomUser.user.birthday;
 
           this.audio = new Howl({
-          //TODO - URL of current Users lieblingssong
+            //TODO - URL of current Users lieblingssong
+
             src: ['https://audio-ssl.itunes.apple.com/itunes-assets/Music1/v4/f1/1c/93/f11c9317-50bb-20bb-f76f-5e4289b52663/mzaf_6202006266019995023.plus.aac.p.m4a'],
           });
 
@@ -54,14 +67,14 @@ export class SongSwipingComponent implements OnInit {
       );
   }
 
+
   //pauses song, when it plays, plays song when it's paused
   pausePlaySong(): void {
     if (this.isPlaying) {
       this.audio.pause();
       this.isPlaying = false;
       this.changeButtonPause();
-      }
-    else {
+    } else {
       this.audio.play();
       this.isPlaying = true;
       this.changeButtonPlay()
@@ -72,7 +85,9 @@ export class SongSwipingComponent implements OnInit {
 
   repeatSong(): void {
     this.audio.stop();
-    if (this.isPlaying==false) { this.changeButtonPlay()}
+    if (this.isPlaying == false) {
+      this.changeButtonPlay()
+    }
     this.isPlaying = true;
     this.audio.play();
     console.log("User called repeatSong()")
@@ -82,36 +97,41 @@ export class SongSwipingComponent implements OnInit {
   hateSong(): void {
     console.log("User called hateSong()");
     this.songService.sethate(this.randomUser.user.id);
-    if (this.isPlaying==false) { this.changeButtonPlay()}
+    if (this.isPlaying == false) {
+      this.changeButtonPlay()
+    }
     this.audio.stop();
     this.ngOnInit()
   }
 
-  loveSong(): void {this.songService.setlove(this.randomUser.user.id)
-    .subscribe(
-      data => {
-        let userMatch = data;
-        console.log("User called loveSong");
-        console.log(userMatch);
-        console.log("This was the Result")
-        this.songService.setlove(this.randomUser.user.id);
-        if (this.isPlaying==false) { this.changeButtonPlay()}
-        this.audio.stop();
-        this.ngOnInit()
-      },
-      err => console.error(err),
-      () => console.log('done getting love')
-    );
+  loveSong(): void {
+    this.songService.setlove(this.randomUser.user.id)
+      .subscribe(
+        data => {
+          let userMatch = data;
+          console.log("User called loveSong");
+          console.log(userMatch);
+          console.log("This was the Result");
+          this.songService.setlove(this.randomUser.user.id);
+          if (this.isPlaying == false) {
+            this.changeButtonPlay()
+          }
+          this.audio.stop();
+          this.ngOnInit()
+        },
+        err => console.error(err),
+        () => console.log('done getting love')
+      );
 
   }
 
   //HTML changing methods
-  changeButtonPause(): void{
+  changeButtonPause(): void {
     document.getElementById("Pause/Play").innerHTML = "Play";
     document.getElementById("Pause/PlayIcon").setAttribute("name", "play")
   }
 
-  changeButtonPlay() : void {
+  changeButtonPlay(): void {
     document.getElementById("Pause/Play").innerHTML = "Pause";
     document.getElementById("Pause/PlayIcon").setAttribute("name", "pause")
   }
@@ -127,9 +147,10 @@ export class SongSwipingComponent implements OnInit {
           role: 'cancel',
           handler: () => {
             console.log('Closed Infotoast');
-          }}
+          }
+        }
 
-          ],
+      ],
       color: "primary",
       position: "middle",
 
@@ -138,11 +159,11 @@ export class SongSwipingComponent implements OnInit {
   }
 
   //content of the information-toast
-  createMessage()  {
+  createMessage() {
     //TODO - Wenn implementiert, das Kommentar hinzufügen
-   //let comment= this.randomUser.comment
+    //let comment= this.randomUser.user.comment
     let comment = "Ich verbinde mit dem Lied so viele Erinnerungen an meine Zeit als Hund!";
     return comment + "<br> <br> <strong> Songtitle: </strong>" + this.randomUser.songName +
-      "<br> <strong> Genre: </strong>" +this.randomUser.genre;
+      "<br> <strong> Genre: </strong>" + this.randomUser.genre;
   }
 }
